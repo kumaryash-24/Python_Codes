0951ab253facd5c6002e3855f33e9b92df08d4f0
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2024-12-28 19:34:15 +0530

    Create add_number.py

---
+++ add_number.py
@@ -0,0 +1,16 @@
+num1 = int(input('enter a number: '))
+num2 = int(input('enter a number: '))
+sum = num1 + num2
+print(f"the sum of {num1} and {num2} is:", sum)
+
+
+# using function
+def addNum():
+    num1 = int(input('enter a number: '))
+    num2 = int(input('enter a number: '))
+    
+    sum_res = num1 + num2
+    
+    print("the sum of two numbers:", sum_res)
+    
+addNum()
