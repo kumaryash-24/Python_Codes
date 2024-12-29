719c04e882e2fc5d59ae80be81d675b6fa169da1
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2024-12-29 18:44:09 +0530

    Create sq_root.py

---
+++ sq_root.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the number: "))
+res = num1 ** (0.5)   # or also  num1 ** (1/2) 
+print(res)
+
+# using function
+def sq():
+    num1 = int(input("Enter a number: "))
+    res = num1 ** (1/2)
+    print(res)
+sq()
