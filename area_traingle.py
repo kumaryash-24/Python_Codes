2a7b8457320903126a2eaac737e32270cf0d4ab2
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2024-12-30 19:59:42 +0530

    Create area_traingle.py

---
+++ area_traingle.py
@@ -0,0 +1,18 @@
+base = float(input("Enter the base of a triangle: "))
+height = float(input("Enter the height of a traingle: "))
+
+area = (0.5) * base * height  # or 1/2 *base*height
+print("The Area of Triangle is:", area)
+
+#using function
+def area_triangle():
+    base = float(input("Enter the base of a triangle: "))
+    height = float(input("Enter the height of a triangle: "))  #
+    
+    area = (0.5) * base * height
+    print("The Area of the Triangle is:", area)
+
+area_triangle()
+
+    
+    
