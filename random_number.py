a872562aca600011c4b1ce1e899d4162d21f1bba
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-01-07 22:45:31 +0530

    Create random_number.py

---
+++ random_number.py
@@ -0,0 +1,16 @@
+# using random.randint()
+import random 
+num = random.randint(1,6)
+print(num)
+
+
+# using random.choice([])
+import random
+num = random.choice([1, 2, 3, 4, 5, 6])
+print(num)
+
+
+# using random.randrange()
+import random
+num = random.randrange(1,7) # The upper limit (7) is exclusive, so it goes up to 6.
+print(num)
