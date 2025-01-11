a94e0ff7b3c99127638985d73765a848e3a1d278
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-01-11 17:58:45 +0530

    Create Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+for i in range(1,11):
+  print(num, "*", i, "=",  num*i)  # print(num*i) it will print only 2,4,6,8........20
+  
+  
+# using while loop
+num  = int(input("Enter a number: "))
+i = 1
+while i<=10:
+    print(num, "*", i, "=", num*i)
+    i += 1
