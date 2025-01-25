f5ff7e442093ed6899dc3a77679d32f1e7856913
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-01-25 21:03:58 +0530

    Create Factorial_recurr.py

---
+++ Factorial_recurr.py
@@ -0,0 +1,13 @@
+def fact(a):
+    if a == 0:
+        return 1
+    else:
+        return ((a) * fact(a-1))
+    
+num = int(input("Enter a number: "))
+result = fact(num)
+print(f"The factorial of {num} is", result)
+
+
+
+
