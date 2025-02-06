1e9574e7f86dc1d515ec066e9b3b895aaebd71b9
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-02-06 22:25:18 +0530

    Update Second_small_arr.py

--- Second_small_arr.py
+++ Second_small_arr.py
@@ -6,7 +6,7 @@ second = math.inf
 for i in range(0, len(arr)):
    if arr[i] < first:
      first = arr[i]
-
+ 
 for i in range(0, len(arr)):
    if arr[i] != first and arr[i] < second:
      second = arr[i]
