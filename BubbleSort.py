3a25bcaad8811eba2811ce8f275467d7871ecff6
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-12 22:51:03 +0530

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,32 @@
+size = int(input("Enter a size of the list: "))
+a = []
+for i in range(size):
+    val = int(input("Enter a number: "))
+    a.append(val)
+
+for i in range(size):
+    for j in range(0,size-i-1):
+        if(a[j] > a[j+1]):
+            a[j], a[j+1] = a[j+1], a[j]
+
+print("Sorted List is: ")
+print(a) 
+
+
+# using function
+def bubble_sort(a, size):
+    for i in range(size):
+        for j in range(0, size - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+size = int(input("Enter the size of the list: "))
+a = []
+for i in range(size):
+    val = int(input("Enter a number: "))
+    a.append(val)
+
+
+bubble_sort(a, size)
+print("Sorted List is:")
+print(a)
