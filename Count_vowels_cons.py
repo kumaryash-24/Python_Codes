58e34bb9b707396b69d5968d059c14fa548a0236
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-14 18:59:16 +0530

    Update Count_vowels_cons.py

--- Count_vowels_cons.py
+++ Count_vowels_cons.py
@@ -3,7 +3,7 @@ vowel = 0
 cons = 0
 for i in range(0, len(a)):
     if (a[i] == "a" or a[i] == "e" or a[i] == "i" or a[i] == "o" or a[i] == "u" or
-        a[i] == "A" or a[i] == "E" or a[i] == "I" or a[i] == "O" or a[i] == "U"):
+        a[i] == "A" or a[i] == "E" or a[i] == "I" or a[i] == "O" or a[i] == "U"):   
         vowel = vowel + 1
     else:
         cons = cons + 1
