1bedca3be5b986364620239d17d8939b130be92f
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-25 21:47:12 +0530

    Update HCF.py

--- HCF.py
+++ HCF.py
@@ -1,7 +1,7 @@
 # HCF of Two Numbers
 # Here, in this section we will discuss how to find HCF of two numbers in python. HCF means (Highest Common Factor)
 # also known as GCD (Greatest Common Divisor).
-
+     
 # x is called HCF of a & b two conditions :
 
 # x can completely divide both a & b leaving remainder 0
