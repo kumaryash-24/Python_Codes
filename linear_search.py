0d586f214a43148049f58ec641d77938f72d709d
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-26 20:50:46 +0530

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -8,7 +8,7 @@ arr = list(map(int,input("Enter the elements: ").split()))
 target = int(input("Enter the no that u want to search: "))
 # print(linear(arr,target))
 result = linear(arr,target)
-if result != -1:
+if result != -1:    
     print(f"item found at index {result}")
 else:
     print("Not found")
