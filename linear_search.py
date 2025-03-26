c4e170b94d3e805879ff17f0caf4210ac8c3a742
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-26 20:50:40 +0530

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -4,7 +4,7 @@ def linear(arr,target):
             return i  
     return -1
 
-arr = list(map(int,input("Enter the elements: ").split()))
+arr = list(map(int,input("Enter the elements: ").split()))     
 target = int(input("Enter the no that u want to search: "))
 # print(linear(arr,target))
 result = linear(arr,target)
