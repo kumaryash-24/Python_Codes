b05da80baa404dce397855e1442e78f763ab510f
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-27 12:45:06 +0530

    Update Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -15,3 +15,15 @@ wrd2 = "silent"
 print(is_anagram(wrd1, wrd2))
 
 
+# best One
+def is_anagram(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+    
+if is_anagram(word1, word2):
+    print("Anagram ✅")
+else:
+    print("Not an Anagram ❌")
