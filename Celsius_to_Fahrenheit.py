425eb1247a8ec23de9460f786af64423462d67c2
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-06-09 22:24:34 +0530

    Update Celsius_to_Fahrenheit.py

--- Celsius_to_Fahrenheit.py
+++ Celsius_to_Fahrenheit.py
@@ -6,7 +6,7 @@ print("The converted value is", far, "Fahrenheit")
 
                    
 # fahrenheit to celcius = 5/9 * (F - 32)      
-        
+          
 fahrenheit = float(input("Enter a temperature in fahrenheit: "))
 celcius = 5/9 * (fahrenheit-32)
 print("The converted value is", celcius, "Celcius")   
